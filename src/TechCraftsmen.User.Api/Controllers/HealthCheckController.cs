3df4b7aa650efd8f58814e3026ee957a8a55eef8
Artur Rios <arturdev@duck.com>
2024-05-31 16:50:21 -0300

    fix(health check controller): remove unecessary authorize annotation

--- src/TechCraftsmen.User.Api/Controllers/HealthCheckController.cs
+++ src/TechCraftsmen.User.Api/Controllers/HealthCheckController.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using TechCraftsmen.User.Api.Authorization;
 using TechCraftsmen.User.Api.Controllers;
 using TechCraftsmen.User.Core.Dto;
 
 namespace TechCraftsmen.User.Controllers
 {
     [ApiController]
-    [Authorize]
     [Route("[controller]")]
     public class HealthCheckController(ILogger<HealthCheckController> logger) : BaseController(logger)
     {
