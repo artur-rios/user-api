ee17c04bf7c961610ae5aeed864fab5a60eb7d3a
Artur Rios <arturdev@duck.com>
2024-06-18 00:20:01 -0300

    chore: remove unecessary using

--- src/TechCraftsmen.User.Api/Authorization/AuthorizeAtribute.cs
+++ src/TechCraftsmen.User.Api/Authorization/AuthorizeAtribute.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using TechCraftsmen.User.Core.Dto;
 
